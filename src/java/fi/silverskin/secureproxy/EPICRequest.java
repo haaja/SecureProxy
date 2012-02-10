ee4dd368e1099fc1359b152a2b23b673c34ad94d
Iivari Äikäs <iivari.aikas@gmail.com>
2012-02-10 17:25:25 +0200

    Added missing constructors to EPIC-classes.

--- src/java/fi/silverskin/secureproxy/EPICRequest.java
+++ src/java/fi/silverskin/secureproxy/EPICRequest.java
@@ -1,5 +1,13 @@
 package fi.silverskin.secureproxy;
 
+import java.util.HashMap;
+
 public class EPICRequest extends EPICAbstraction {
-    
+
+    public EPICRequest() {
+    }
+
+    public EPICRequest(HashMap<String, String> headers, String body) {
+        super(headers, body);
+    }
 }
--- src/java/fi/silverskin/secureproxy/EPICResponse.java
+++ src/java/fi/silverskin/secureproxy/EPICResponse.java
@@ -1,5 +1,13 @@
 package fi.silverskin.secureproxy;
 
+import java.util.HashMap;
+
 public class EPICResponse extends EPICAbstraction {
-    
+
+    public EPICResponse() {
+    }
+
+    public EPICResponse(HashMap<String, String> headers, String body) {
+        super(headers, body);
+    }
 }
