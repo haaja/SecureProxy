9e9a8d3ed3172e90be02dba7f8a5d247a07300d4
Hannu Kärnä <hannu.karna@helsinki.fi>
2012-02-13 09:54:54 +0200

    Test version of hackAndSlashOut implemented

--- src/java/fi/silverskin/secureproxy/HackAndSlash.java
+++ src/java/fi/silverskin/secureproxy/HackAndSlash.java
@@ -1,12 +1,13 @@
 package fi.silverskin.secureproxy;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class HackAndSlash {
 
     private EPICRequest request;
     private EPICResponse response;
-
     //To be replaced with proper settings
     private String remoteUrl = "localhost";
     private String remotePort = "8084";
@@ -33,6 +34,32 @@ public class HackAndSlash {
 
     public EPICResponse hackAndSlashOut(EPICResponse response) {
 
-        throw new NotImplementedException();
+        
+        Pattern tagPattern = Pattern.compile("<(\\s)*img[^>]*>");
+        String oldResponse = response.getBody(), newResponse = "";
+        Matcher tagMatcher = tagPattern.matcher(oldResponse);
+
+        int index = 0;
+        while (tagMatcher.find()) {
+            
+            if (tagMatcher.start() > index) {
+                newResponse += oldResponse.substring(index, tagMatcher.start());
+            }
+            index = tagMatcher.end();
+            Pattern sourcePattern =
+                    Pattern.compile("src(\\s)*=(\\s)*\"[^\"]*\"");
+            Matcher sourceMatcher = sourcePattern.matcher(tagMatcher.group());
+            newResponse += sourceMatcher.replaceFirst("src=\"http://upload.wikimedia.org/wikipedia/"
+                    + "commons/thumb/a/af/Tux.png/220px-Tux.png\"");
+        }
+        if (index == 0) {
+            newResponse = oldResponse;
+        } else if (index < oldResponse.length()) {
+            newResponse +=
+                    oldResponse.substring(index);
+        }
+        response.setBody(newResponse);
+        return response;
     }
+   
 }
