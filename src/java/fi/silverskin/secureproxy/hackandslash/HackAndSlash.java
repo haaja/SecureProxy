cf8b72a009bf575af28814fc9016792d59287ef2
Hannu Kärnä <hannu.karna@helsinki.fi>
2012-02-22 17:10:30 +0200

    Old version of HackAndSlash

--- src/java/fi/silverskin/secureproxy/hackandslash/HackAndSlash.java
+++ src/java/fi/silverskin/secureproxy/hackandslash/HackAndSlash.java
@@ -13,7 +13,6 @@ public class HackAndSlash {
     //To be replaced with proper settings
     private String remoteUrl = "localhost";
     private String remotePort = "8084";
-    
 
     public HackAndSlash() {
         this.request = null;
@@ -36,30 +35,68 @@ public class HackAndSlash {
     }
 
     public EPICResponse hackAndSlashOut(EPICResponse response) {
-        Pattern tagPattern = Pattern.compile("<(\\s)*img[^>]*>");
-        String oldResponse = response.getBody(), 
-               newResponse = "";
-        Matcher tagMatcher = tagPattern.matcher(oldResponse);
 
-        int index = 0;
-        while (tagMatcher.find()) {
-            if (tagMatcher.start() > index)
-                newResponse += oldResponse.substring(index, tagMatcher.start());
-
-            index = tagMatcher.end();
-            Pattern sourcePattern = Pattern.compile("src(\\s)*=(\\s)*\"[^\"]*\"");
-            Matcher sourceMatcher = sourcePattern.matcher(tagMatcher.group());
-            newResponse += sourceMatcher.replaceFirst("src=\"http://upload.wikimedia.org/wikipedia/"
-                    + "commons/thumb/a/af/Tux.png/220px-Tux.png\"");
+        String[][] tagsAndAttributes = new String[2][];
+        String[] tags = {"img", "a", "iframe", "frame", "script","form", "base", "link"};
+        tagsAndAttributes[0] = tags;
+        String[] attributes = {"src", "href", "src", "src","src","action", "href", "href"};
+        tagsAndAttributes[1] = attributes;
+        String oldResponse = response.getBody(), newResponse = "";
+        for (int i = 0; i < tagsAndAttributes[0].length; i++) {
+            newResponse = "";
+            Pattern tagPattern = Pattern.compile("<(\\s)*" + tagsAndAttributes[0][i] + "[^>]*>");
+            Matcher tagMatcher = tagPattern.matcher(oldResponse);
+            int index = 0;
+            while (tagMatcher.find()) {   
+                if (tagMatcher.start() > index) {
+                    newResponse += oldResponse.substring(index, tagMatcher.start());
+                }
+                index = tagMatcher.end();
+                String tag = tagMatcher.group();
+                newResponse += convertUrlInTag(tag, tagsAndAttributes[1][i]);
+            }
+            if (index == 0) {
+                newResponse = oldResponse;
+            } else if (index < oldResponse.length()) {
+                newResponse += oldResponse.substring(index);
+            }
+            oldResponse = newResponse;
         }
-        
-        if (index == 0)
-            newResponse = oldResponse;
-        else if (index < oldResponse.length())
-            newResponse += oldResponse.substring(index);
-        
         response.setBody(newResponse);
         return response;
     }
+
+    public String getPseudoUrl(String URL) {
+        return "pseudoURL";
+    }
+
+    public String convertUrlInTag(String tag, String attributeName) {
+
+        Pattern sourcePattern = Pattern.compile(attributeName + "(\\s)*=(\\s)*\"[^\"]*\"");
+        Matcher sourceMatcher = sourcePattern.matcher(tag.toLowerCase());
+        if (sourceMatcher.find()) {
+            int attributeStart = sourceMatcher.start();
+            int attributeEnd = sourceMatcher.end();
+            String temp = tag.substring(attributeStart, attributeEnd);
+            Pattern urlPattern = Pattern.compile("\"[^\"]+\"");
+            Matcher urlMatcher = urlPattern.matcher(temp);
+            if ((urlMatcher.find())) {
+                String url = urlMatcher.group();
+                url = url.substring(1, url.length() - 1).trim();
+                if (!url.equals("")) {
+                    return tag.substring(0, attributeStart + urlMatcher.start() + 1)
+                            + getPseudoUrl(url) + tag.substring(attributeEnd - 1);
+                }
+            }
+        }
+        return tag;
+    }
     
+    public static void main(String[] args){
+        HackAndSlash hack = new HackAndSlash();
+        String test = "jfhjfh<img  src=\"hkhgdkjgk\">tt<img src=\"  s\">yy<a href=\" y\">";
+        EPICResponse res = new EPICResponse();
+        res.setBody(test);
+        System.out.println(hack.hackAndSlashOut(res).getBody());
+    }
 }
