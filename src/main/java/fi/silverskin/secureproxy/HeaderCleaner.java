acb0a66b9e418b9cd665159b1656c2d19c5d9426
Janne Haapsaari <haaja@iki.fi>
2012-03-12 18:28:44 +0200

    Modified the way the headers are cleaned
    It's now easier to add additional headers to be preserved. Perhaps we can
    put the option into configuration file.

--- src/main/java/fi/silverskin/secureproxy/HeaderCleaner.java
+++ src/main/java/fi/silverskin/secureproxy/HeaderCleaner.java
@@ -7,25 +7,29 @@ import java.util.logging.Logger;
 public class HeaderCleaner {
 
     private static ProxyConfigurer configurer = new ProxyConfigurer();
-    public static final String PROTECTEDHOST = configurer.getConfigure("protectedHost")[0];
-
+    private static final String PROTECTEDHOST = configurer.getConfigure("protectedHost")[0];
     private static final Logger LOGGER = Logger.getLogger(HeaderCleaner.class.getName(), null);
+    private static String[] headersToBePreserved = { "cookie" };
 
-
+    /**
+     * Cleans request headers of everything else except the required ones.
+     * @param request Original HTTP request
+     * @return HTTP request with cleaned headers
+     */
     public static EPICRequest cleanHeaders(EPICRequest request) {
         LOGGER.entering(HeaderCleaner.class.getCanonicalName(), "cleanHeaders", request);
         HashMap<String, String> cleanedHeaders = new HashMap<String, String>();
         Map<String, String> originalHeaders = request.getHeaders();
 
-        // headers to be preserved
-        String cookieHeader = originalHeaders.get("cookie");
-        
-        cleanedHeaders.put("cookie", cookieHeader);
+        for (int i=0; i<headersToBePreserved.length; i++) {
+            cleanedHeaders.put(headersToBePreserved[i],
+                               originalHeaders.get(headersToBePreserved[i]));
+        }
+
         cleanedHeaders.put("host", PROTECTEDHOST);
-        
         request.setHeaders(cleanedHeaders);
-        LOGGER.exiting(HeaderCleaner.class.getName(), "cleanHeaders", request);
         
+        LOGGER.exiting(HeaderCleaner.class.getName(), "cleanHeaders", request);
         return request;
     }
 }
