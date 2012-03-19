8386642266190530b741b3132e319d7b2c7caf2a
Janne Haapsaari <haaja@iki.fi>
2012-03-19 10:52:09 +0200

    Created helper method to read config

--- src/main/java/fi/silverskin/secureproxy/HeaderCleaner.java
+++ src/main/java/fi/silverskin/secureproxy/HeaderCleaner.java
@@ -2,12 +2,13 @@ package fi.silverskin.secureproxy;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 public class HeaderCleaner {
 
-    private static ProxyConfigurer configurer = new ProxyConfigurer("config.properties");
-    private static final String PROTECTEDHOST = configurer.getConfigure("protectedHost")[0];
+    private static final String CONFIGFILE = "config.properties";
+    private static Properties headerCleanerProperties;
     private static final Logger LOGGER = Logger.getLogger(HeaderCleaner.class.getName(), null);
     private static String[] headersToBePreserved = { "cookie" };
 
@@ -17,7 +18,11 @@ public class HeaderCleaner {
      * @return HTTP request with cleaned headers
      */
     public static EPICRequest cleanHeaders(EPICRequest request) {
-        LOGGER.entering(HeaderCleaner.class.getCanonicalName(), "cleanHeaders", request);
+        LOGGER.entering(HeaderCleaner.class.getCanonicalName(),
+                        "cleanHeaders",
+                        request);
+
+        headerCleanerProperties = readConfig();
         HashMap<String, String> cleanedHeaders = new HashMap<String, String>();
         Map<String, String> originalHeaders = request.getHeaders();
 
@@ -26,10 +31,20 @@ public class HeaderCleaner {
             originalHeaders.get(headersToBePreserved[i]));
         }
 
-        cleanedHeaders.put("host", PROTECTEDHOST);
+        cleanedHeaders.put("host",
+                           headerCleanerProperties.getProperty("protectedHost"));
         request.setHeaders(cleanedHeaders);
         
         LOGGER.exiting(HeaderCleaner.class.getName(), "cleanHeaders", request);
         return request;
+    }
+
+    /**
+     * Reads configuration properties using ProxyConfigurer
+     * @return SecureProxy common properties
+     */
+    private static Properties readConfig() {
+        ProxyConfigurer configurer = new ProxyConfigurer(CONFIGFILE);
+        return configurer.getConfigurationProperties();
     }
 }
