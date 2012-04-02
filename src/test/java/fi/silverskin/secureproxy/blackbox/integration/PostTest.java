a92f499942bc9694f7677eb2a6824770dc88db10
Topias Heinonen <corvus@iki.fi>
2012-04-02 16:26:27 +0300

    Added changeable configs for integration tests.
    They now use a system variable secureproxy.integration.config to specify where to look for a config file to use. Added a default config to test/resources and modified netbeans maven actions to use it.

--- src/test/java/fi/silverskin/secureproxy/blackbox/integration/PostTest.java
+++ src/test/java/fi/silverskin/secureproxy/blackbox/integration/PostTest.java
@@ -4,6 +4,7 @@
  */
 package fi.silverskin.secureproxy.blackbox.integration;
 
+import fi.silverskin.secureproxy.ProxyConfigurer;
 import org.junit.*;
 import static org.junit.Assert.*;
 import org.openqa.selenium.*;
@@ -14,27 +15,37 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
  * @author corvus
  */
 public class PostTest {
-        
+
+    String configPath = System.getProperty("secureproxy.integration.config");
+    ProxyConfigurer configurer = new ProxyConfigurer(configPath);
+
     @Test
-    public void PostTest() {
+    public void SimplePostTest() {
+        System.out.println("Config read from: '" + configPath + "'");
+        
         WebDriver driver = new HtmlUnitDriver();
+
+        String publicUri = configurer.getProperty("publicURI");
+        String publicHttpPort = configurer.getProperty("publicHttpPort");
         
-        driver.get("http://localhost:8084/post-form.php");
+        String url = publicUri + ":" + publicHttpPort + "/post-form.php";
         
-        System.out.println("Page title is: " + driver.getTitle());
+        System.out.println("Fetching page: '" + url + "'");
         
+        driver.get(url);
+
+        System.out.println("Before POST, page title is: '" + driver.getTitle() + "'");
+
         WebElement element = driver.findElement(By.name("input"));
-        
+
         String testInput = "testi";
-        
+
         element.sendKeys(testInput);
-        
+
         element.submit();
-        
-        System.out.println("Page title is: " + driver.getTitle());
-        
+
+        System.out.println("After POST, page title is: '" + driver.getTitle() + "'");
+
         assertEquals(testInput, driver.getTitle());
     }
-    
-    
 }
